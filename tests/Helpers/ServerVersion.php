7425066254145352539de3b3f34c66a30639e388
Mateusz <matt.wozakowski@gmail.com>
2024-01-31 16:32:21 +0000

    Add testing

--- tests/Helpers/ServerVersion.php
+++ tests/Helpers/ServerVersion.php
@@ -284,6 +284,11 @@ class ServerVersion
         return ($this->major == 7 && $this->minor >= 6) || $this->major > 7;
     }
 
+    public function supportsScopeSearchIndexes(): bool
+    {
+        return ($this->major == 7 && $this->minor >= 6) || $this->major > 7;
+    }
+
     /**
      * @return int
      */
